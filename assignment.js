cf513aa193d8d1fd55462df835e2c30da787ad88
Kaibolt <kyle.salowitz08@gmail.com>
2024-10-25 12:27:13 -0400

    adf

--- assignment.js
+++ assignment.js
@@ -1,3 +1,4 @@
+/*
 function getSpaces(spaces, yesterday, today) {
   let count = 0;
   for (let i = 0; i < spaces; i++) {
@@ -7,4 +8,26 @@ function getSpaces(spaces, yesterday, today) {
   }
   return count;
 }
-console.log(getSpaces(5, "CC..C", ".CCCC"));
+
+console.log(getSpaces(5, "CC..C", ".CCCC"));*/
+function ntB(n) {
+  let p7,
+    p6,
+    p5,
+    p4,
+    p3,
+    p2,
+    p1,
+    p0 = 0;
+  let t1 = 128;
+  for (let i = 0; i < 7; i++) {
+    if ((n = t1 || n < t1)) {
+      p7++;
+      n = n - t1;
+      t1 = t1 / 2;
+    }
+  }
+  return p0, p1, p2, p3, p4, p5, p6, p7;
+}
+
+console.log(ntB(128));
---
+++ notes.js
@@ -0,0 +1,11 @@
+function binaryToDec(binary) {
+  let decimal = 0;
+
+  for (let i = 0; i < binary.length; i++) {
+    if (binary[binary.length - 1 - i] === "1") {
+      decimal = decimal + Math.pow(2, i);
+    }
+  }
+  return decimal;
+}
+console.log(binaryToDec("1101"));
